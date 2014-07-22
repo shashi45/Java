13802eed2c1d0ad8ed1e4677cd813c85a7c35a2f
Shashi <shashinece@gmail.com>
2014-07-22 00:40:44 -0400

    adding the CoreJavaPOCs project to the repo.

---
+++ CoreJavaPOCs/src/myclasses/demos/corejava/string/StringApp.java
@@ -0,0 +1,12 @@
+package myclasses.demos.corejava.string;
+
+public class StringApp {
+	public static void main(String[] args) {
+		//Strings are immutable objects, that means once created and can never be changed.
+		//Strings are objects and can be instantiated just like any other class.
+		String newString = new String("firstString");
+		//A String can also assigned a value using string literals.
+		String stringLit = "String literals";
+		//There is a subtle difference in the above two ways of creation.
+	}
+}
