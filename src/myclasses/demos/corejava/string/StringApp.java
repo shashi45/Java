dad2fc80926e32fe3e019e9d41e5a9faf3256e01
Shashi <shashinece@gmail.com>
2014-07-26 23:06:27 -0400

    updating the StringApp class.

--- src/myclasses/demos/corejava/string/StringApp.java
+++ src/myclasses/demos/corejava/string/StringApp.java
@@ -9,32 +9,36 @@ public class StringApp {
 		// just to support rich international set of characters.).
 		// Strings are immutable objects, that means once created and can never
 		// be changed.
-//------------
-		//Section 1
-		//Creating a new String
+		// ------------
+		// Section 1
+		// Creating a new String
 		{
-		// Strings objects can be instantiated just like any other objects .
-		String newString = new String("firstString"); // 1
-		// A String can also assigned a value using string literals.
-		String stringLit = "String literal"; // 2
-		// There is a subtle difference in the above two ways of creation.
-		// 1) creates a new string every time and that too in the heap section
-		// of the memory but not in the
-		// permgen space(String pool).
-		// 2) if the string already presents in the string pool it just assigns
-		// the reference,
-		// else creates a new string in the string pool and assigns it to the
-		// string variable.
-		newString.intern(); // 3
-		// 3) Adds the string to the string pool.
+			// Strings objects can be instantiated just like any other objects .
+			String newString = new String("firstString"); // 1
+			// A String can also assigned a value using string literals.
+			String stringLit = "String literal"; // 2
+			// There is a subtle difference in the above two ways of creation.
+			// 1) creates a new string every time and that too in the heap
+			// section
+			// of the memory but not in the
+			// permgen space(String pool).
+			// 2) if the string already presents in the string pool it just
+			// assigns
+			// the reference,
+			// else creates a new string in the string pool and assigns it to
+			// the
+			// string variable.
+			newString.intern(); // 3
+			// 3) Adds the string to the string pool.
 
-		// Note: We said string objects are immutable, but how about string
-		// references?
-		// Answer is string references are not immutable, that means we can
-		// point the reference to any string object without actually creating or
-		// manipulating a string.
-	}
-//-----------------------------		
+			// Note: We said string objects are immutable, but how about string
+			// references?
+			// Answer is string references are not immutable, that means we can
+			// point the reference to any string object without actually
+			// creating or
+			// manipulating a string.
+		}
+		// -----------------------------
 		// Section 2
 		{
 			// String concat method
@@ -71,7 +75,7 @@ public class StringApp {
 			// assigning to any variable and it is lost instantly.
 			// We are trying to print s2 which is still referring to "Hello"
 			// object.
-// ------------------------------------------
+			// ------------------------------------------
 			// String concatenation using '+'
 
 			// We can concatenate two strings using '+' operator too and that
@@ -121,7 +125,7 @@ public class StringApp {
 			System.out.println("Output of 1+two is " + s6);
 
 			// the above example is adding an integer 1 to string two.
-//------------
+			// ------------
 			// SubSection 1
 			{
 				// The main difference between concat and '+' is String concat
@@ -144,53 +148,114 @@ public class StringApp {
 				s11.concat("fall ");
 				s22.concat(s11);
 				s11 += "winter ";
-				//Guess the output
+				// Guess the output
 				System.out.println(s11 + " " + s22);
-				
+
 			}
-			//Just like what happened with concat method, toLowerCase() and toUpperCase() methods creates a new string object by making the characters to 
+			// Just like what happened with concat method, toLowerCase() and
+			// toUpperCase() methods creates a new string object by making the
+			// characters to
 			// either lower case or upper case letters.
-			//Example 
+			// Example
 			String s8 = "java";
 			s8.toUpperCase();
-			System.out.println("converting the lower case of s8 to uppercase without assigning the output to any reference variable: "+ s8);
+			System.out
+					.println("converting the lower case of s8 to uppercase without assigning the output to any reference variable: "
+							+ s8);
 			s8 = s8.toUpperCase();
-			System.out.println("converting the lower case of s8 to uppercase and assigning the output to same variable: "+ s8);
-			//Observer the output of both the above cases.
-			
+			System.out
+					.println("converting the lower case of s8 to uppercase and assigning the output to same variable: "
+							+ s8);
+			// Observer the output of both the above cases.
+
 		}
-//--------------------------
-		//Section 3 
+		// --------------------------
+		// Section 3
 		{
-			//Other string methods.
-			
-			//replace(char old, char new)
+			// Other string methods.
+
+			//
+
+			// replace(char old, char new)
 			String s9 = "JaVa";
 			s9 = s9.replace('a', 'A');
-			System.out.println("repacing 'a' with 'A' in string JaVa: "+ s9);
-			
-			//    charAt(int index)
+			System.out.println("repacing 'a' with 'A' in string JaVa: " + s9);
+
+			// charAt(int index)
 			String s10 = "java";
-			System.out.println("char at 2: "+ s10.charAt(2));
-			
-			//equalsIgnoreCase(String s)
+			System.out.println("char at 2: " + s10.charAt(2));
+
+			// equalsIgnoreCase(String s)
 			String s11 = "Java";
-			System.out.println("Ignoring cases: "+ s11.equalsIgnoreCase("JAVA"));
-			
-			//length()
-			//String class has a separate method which returns the length of the string.
+			System.out.println("Ignoring cases: "
+					+ s11.equalsIgnoreCase("JAVA"));
+
+			// length()
+			// String class has a separate method which returns the length of
+			// the string.
 			String s12 = "java";
-					System.out.println("Length of the string: "+ s12.length());
-			//toString()
-			// Might be wondering why there toString() method in String class which would be doing nothing, 
-			// That is becasue since all the objects have to invoke string method for ,
-		
+			System.out.println("Length of the string: " + s12.length());
+			// toString()
+			// Might be wondering why there toString() method in String class
+			// which would be doing nothing but to print the same string object
+			// on which we are calling.
+			// That is becasue just to maintain consistancy, since all the
+			// objects have to invoke
+			// tostring method for meaningful representation of the object.
+			// trim()
+			// removes the leading or trailing whitespaces of the string.
+			String s13 = " Java ";
+			System.out
+					.println("String before whitespace-->" + s13 + "<-- "
+							+ "String after whitespaces trimed-->" + s13.trim()
+							+ "<--");
+
+			// toCharArray();
+			// This method returns the new character array which would be the copy of the original string's char array,
+			// whose length is the length of the string.
+
+			//*****In string implementation every string is treated as a sequence of characters.
+
+			// This method inturn uses another method which servers the same purpose which is getChars(...) only
+			//difference is the arguments, check the example below;
+		String s14 = "toCharArray";
+		char[] cArray = s14.toCharArray();
+		for(char ch : cArray){
+			System.out.print(ch+" ");
 		}
-		String s13 = "Java";
-		char[] c = new char[s13.length()];
-		c=s13.toCharArray();
-		s13.getChars(0, 4, c, 0);
-		System.out.println(" java: "+ c[1]);
-		System.out.println("10 * 5 "+ 10*5);
+
+		// getChars(...);
+		// i) getChars(char dst[], int dstBegin);
+		// copies the string chars startign from the dstBegin.
+
+		// getChars(int srcBegin, int srcEnd, char dst[], dstBegin);
+		// The arguments for this method are
+		// srcBegin: index of the start of the char to be copied.
+		// srcEnd: 	index of the end of the char to be copied.
+		//dst: is the destination array.
+		String s15= "getChars";
+		char[] newChar = new char[s15.length()];
+		s14.getChars(0,s15.length(),newChar,2);
+		for(char ch : newChar){
+			System.out.print(ch+" ");
+		}
+
+		}
+
+		// ---------------------------
+		//section 4
+		//subString();
+		{
+			//What is subString and how it works in java.
+			//SubString methods returns us a part of the string based on the boundaries we mention.
+
+		}
+
+		String s131 = "Java";
+		char[] c = new char[s131.length()];
+		c = s131.toCharArray();
+		s131.getChars(0, 4, c, 0);
+		System.out.println(" java: " + c[1]);
+		System.out.println("10 * 5 " + 10 * 5);
 	}
 }
