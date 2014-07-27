c0141da58bf0a3a0dbe8ec90a7a851cd654b2216
Shashi <shashinece@gmail.com>
2014-07-27 16:39:57 -0400

    Updating StringApp and ReadMe file.

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,53 @@
 Core Java demos.
 ================
+
+StringApp.java
+==============
+The contents in the file are divided into sections,
+
+1) Section 1
+This section explains the following,
+What is a String object?
+How many ways to create it?
+What are the differences between them?
+
+2) Section 2
+
+How many ways to concat a string? (concat method and + operator)
+What are the differences?
+What would be the output when you add other data types to a string using '+' operator
+
+3) Section 3
+
+Other important methods like
+
+* replace
+* charAt
+* equalsIgnoreCase
+* length
+* trim
+* toCharArray
+* getChars
+
+
+4) Section 4
+
+What is a substring?
+How it works?
+How memory leaks in string occurs?
+Is there any work around in handling memory leaks which occurs when using substring?
+If so how can you achieve it?
+
+5) Section 5
+
+What are the two API classes we have if we have to make lot of modifications to a String object?
+What are the difference between String and the other two API classes?(StringBuffer and StringBuilder)
+Other important methods in StringBuffer and StringBuilder.
+
+
+*******
+StringApp.md ReadMe file contains the following,
+
+* Strings and its Memory.
+* Why String is an immutable class?
+* What are the implications if String is not immutable?
--- src/myclasses/demos/corejava/string/StringApp.java
+++ src/myclasses/demos/corejava/string/StringApp.java
@@ -3,12 +3,10 @@ package myclasses.demos.corejava.string;
 public class StringApp {
 	public static void main(String[] args) {
 		// Strings are a sequence of characters.
-		// This being said strings are backed by the char array in the
-		// implementation.
+		// This being said strings are backed by the char array in the implementation.
 		// Each character in the string is of size 16 bit(char is 16 bit in java
 		// just to support rich international set of characters.).
-		// Strings are immutable objects, that means once created and can never
-		// be changed.
+		// Strings are immutable objects, that means once created and can never be changed.
 		// ------------
 		// Section 1
 		// Creating a new String
@@ -18,25 +16,16 @@ public class StringApp {
 			// A String can also assigned a value using string literals.
 			String stringLit = "String literal"; // 2
 			// There is a subtle difference in the above two ways of creation.
-			// 1) creates a new string every time and that too in the heap
-			// section
-			// of the memory but not in the
-			// permgen space(String pool).
-			// 2) if the string already presents in the string pool it just
-			// assigns
-			// the reference,
-			// else creates a new string in the string pool and assigns it to
-			// the
-			// string variable.
+			// 1) creates a new string every time and that too in the heap section
+			// of the memory but not in the permgen space(String pool).
+			// 2) if the string already presents in the string pool it just assigns  the reference,
+			// else creates a new string in the string pool and assigns it to the string variable.
 			newString.intern(); // 3
 			// 3) Adds the string to the string pool.
 
-			// Note: We said string objects are immutable, but how about string
-			// references?
+			// Note: We said string objects are immutable, but how about string references?
 			// Answer is string references are not immutable, that means we can
-			// point the reference to any string object without actually
-			// creating or
-			// manipulating a string.
+			// point the reference to any string object without actually creating or manipulating a string.
 		}
 		// -----------------------------
 		// Section 2
@@ -50,18 +39,15 @@ public class StringApp {
 			// "string1 concatenating with strings2".
 			// *****But the String class is an immutable class, then how can it
 			// concatenate one string with other?
-			// s1 = s1.concat(" concatenating with string2") is not merely
-			// adding s1
+			// s1 = s1.concat(" concatenating with string2") is not merely adding s1
 			// to the other string. But the JVM creates two more new string
 			// objects one is " concatenating with string2" and one more
-			// new string which is final concatenated string,
-			// "string1 concatenating with string2".
+			// new string which is final concatenated string, "string1 concatenating with string2".
 			// That means there are total of 3 strings
 			// i) "string1"
 			// ii) " concatenating with string2"
 			// iii) "string1 concatenating with string2"
-			// ******The perfect example to see the immutability of the string
-			// is from below example...
+			// ******The perfect example to see the immutability of the string is from below example...
 
 			String s2 = "Hello";
 			s2.concat(" World");
@@ -69,15 +55,13 @@ public class StringApp {
 
 			// The above statement prints Just Hello, this is because since
 			// String is immutable concat method is not
-			// just adding string "World" to string "Hello" but creating a new
-			// string as we discussed above,
+			// just adding string "World" to string "Hello" but creating a new string as we discussed above,
 			// so above we are creating a new String "Hello World" but not
 			// assigning to any variable and it is lost instantly.
-			// We are trying to print s2 which is still referring to "Hello"
-			// object.
+			// We are trying to print s2 which is still referring to "Hello" object.
 			// ------------------------------------------
 			// String concatenation using '+'
-
+//******** + operator is internally implemented using either by StringBuffer of StringBuilder.
 			// We can concatenate two strings using '+' operator too and that
 			// can be done as follows
 
@@ -87,9 +71,7 @@ public class StringApp {
 					.println("String concatenation using '+' operator: " + s3);
 
 			// String concat v/s '+' operator
-			// A string can be concatenated either using string.concat() method
-			// or
-			// '+' operator.
+			// A string can be concatenated either using string.concat() method or '+' operator.
 			// Their are subtle differences between both.
 
 			// We can add anything to a string using + operator.
@@ -223,7 +205,7 @@ public class StringApp {
 		for(char ch : cArray){
 			System.out.print(ch+" ");
 		}
-
+System.out.println("");
 		// getChars(...);
 		// i) getChars(char dst[], int dstBegin);
 		// copies the string chars startign from the dstBegin.
@@ -232,14 +214,25 @@ public class StringApp {
 		// The arguments for this method are
 		// srcBegin: index of the start of the char to be copied.
 		// srcEnd: 	index of the end of the char to be copied.
-		//dst: is the destination array.
+		// dst: is the destination array.
+		// dstBegin: start index of the destination array.
+
+		/**
+		* srcBegin >= 0
+		* srcEnd   <= string lenght
+		* dstBegin >= 0
+		* dstBegin <= dstBegin+(srcBegin - srcEnd)
+		* throws an IndexOutOfBoundException if the above conditions are not met.
+		*/
+
 		String s15= "getChars";
-		char[] newChar = new char[s15.length()];
-		s14.getChars(0,s15.length(),newChar,2);
+		char[] newChar = new char[s15.length()+2];
+		s15.getChars(0,s15.length(),newChar,2);
+
 		for(char ch : newChar){
 			System.out.print(ch+" ");
 		}
-
+System.out.println("");
 		}
 
 		// ---------------------------
@@ -248,14 +241,95 @@ public class StringApp {
 		{
 			//What is subString and how it works in java.
 			//SubString methods returns us a part of the string based on the boundaries we mention.
+			//	There are two overloaded methods in the substring()
+			// i) substring(beginIndex); Which takes beginIndex and prints the substring starts from begin
+			// index to end of the string.
+			// ii) subString(beginIndex, endIndex); returns a substring starts at the beginIndex and
+			//  ends at endIndex.
+
+			/**
+			* beginIndex: Inclusive
+			* endIndex  : Exclusive
+			* beginIndex >= 0
+			* endIndex   <= string length
+			* beginIndex < endIndex
+			*/
+			//What happens if the beginIndex is equal to the length of the string.
+			String s16 = "JavaSubString";
+			System.out.println("subStr1: "+ s16.substring(s16.length()));
+			// In the above print statement the substring's beginIndex is equal to the length of the string and output is an empty string.
+
+			System.out.println("subStr1: "+ s16.substring(4));// i)
+			System.out.println("subStr2: "+ s16.substring(0,s16.length())); // ii)
+
+			// i) The first print statement prints the substring starting from 4 and end of the string.
+			// ii) the second print statemnt prints the substring starting from 0 to end of the string.
+
+			// We have seen the behavior of the substring method, but How does it works?
+			// If we seen the Implementation class, it always creates a new string, this is because of the fact that string is an immutable class.
+			// Again while creating the new substring it makes use of the value[] which is used to store the original character array.
+			// What is wrong in using the original array here, irrespective of how small is our substring array it will still be allocated the same amount of
+			// memory space as that of the original array. If our original array is too big then our substring will end up having the same space.This causes memory leaks.
+			// And the worst part of all is the original string wont be available for garbage collection too.
+			// ***** There is still a work around for this, we can trim the empty spaces by passing the substring into one of the strings class constructor, which has the
+			// capability to do this. It checks if the array size is greater than the string size, if so it trims by copying the array.
+
+			String s17 = new String(s16.substring(4));
+			System.out.println("Substring using work around: "+ s17);
+
+			// Another way is to call the intern method on the substring which will creates a fetch the substring from the string pool and if it not there it creates a
+			// new string substring it the pool.
+
+			System.out.println("Internalizing the substring: "+ s16.substring(4).intern());
+			// Strings intern methods puts the string in the string pool
+		}
+
+		// Section 5
+		// StringBuffer and StringBulder
+		{
+			// As we were discussing the string class is immutable, that means once created we cannot change it.
+			// But what if we have to make lot of modifications to the string, in this scenario we cannot use string,
+			// since we end up created multiple string objects.
+			// 	So if we have to do lot of string modifications we can use StringBuffer and StringBulder classes.
+			// These have some methods which helps in changing the contents of the string.
+
+			// ****** The common use of these classes is in File I/O's where we have to handle large blocks of strings.
+
+			// So why two classes StringBuffer and StringBuilder
+			// The main difference between these two is
+			// StringBuffer 														StringBufilder
+			//Thread-Safe(Methods are synchronized)			Not Thread-Safe(Methods are not synchronized)
+
+
+			//Important methods in StringBuffer and StringBuilder
+
+			StringBuffer sb = new StringBuffer("Core");
+			sb.append("Java");
+			System.out.println("StringBuffer: "+ sb);
+			// Unlike the String, append method just appends to the existing string instead of creating a new string object every time.
+
+			//delete(strIndex, endIndex);
+			// strIndex: inclusive
+			// endIndex: endIndex
+
+			StringBuffer sb2 = new StringBuffer("012345");
+			sb2.delete(0, 3);
+			System.out.println("delete 0 to 3: "+ sb2);
+
+			//insert(offset, String)  ----Overloaded method, offsetExclusive.
+			StringBuffer sb3 = new StringBuffer("CoreJava");
+			sb3.insert(4, " ");
+			System.out.println("Insert space before Java: "+ sb3);
+			//reverse()
+			StringBuffer sb4 = new StringBuffer("Java");
+			System.out.println("reverse: "+ sb4.reverse());
+
+			//toString
+			StringBuffer sb5 = new StringBuffer("toString");
+			System.out.println("toString returns string object: "+sb5.toString());
 
 		}
 
-		String s131 = "Java";
-		char[] c = new char[s131.length()];
-		c = s131.toCharArray();
-		s131.getChars(0, 4, c, 0);
-		System.out.println(" java: " + c[1]);
-		System.out.println("10 * 5 " + 10 * 5);
+
 	}
 }
